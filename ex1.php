cc773364f00f54fdf089ae3355097ab55b7176a9
crysthiano <crysthianophp@gmail.com>
2014-02-17 07:02:22 -0300

    teste teste layouts

--- ex1.php
+++ ex1.php
@@ -1,6 +1,32 @@
-
 <!DOCTYPE html>
-<html lang="pt">
-    <head></head>
+<html>
+<head>
+	<title>html5 e css3</title>
+	<!--<link type="text/css" href="css/templates.css" rel="stylesheet" media="screen" >-->
+	<!--<link type="text/css" href="css/templates.css" rel="stylesheet" media="not screen and (orientation: portrait)" >-->
+	<!--<link type="text/css" href="css/templates.css" rel="stylesheet" media="screen and (orientation: portrait) and (min-width: 800px)" >-->
+	<!--<link type="text/css" href="css/templates.css" rel="stylesheet" media="screen and (orientation: portrait) and (min-width: 800px),
+	      projection" >-->
+	      
+	<style>
+		@import url("css/templates.css") screen and (max-width: 460px);
+		
+		a {
+			float: left;
+			height: 40px;
+			line-height: 40px;
+			padding-left: 0.8em;
+			padding-right: 0.8em;
+			border-top-left-radius: 8px;
+			border-top-right-radius: 8px;
+			background-color: #42c264;
+			background-image: -moz-linear-gradient(#4FEC50, #42C264);
+			background-image: lenear-gradient(#4FEC50, #42C264);
+		}
+	</style>
+</head>
+<body>
+	<a href=#>Box Title</a>
+	<h1>html5 e css3</h1>
+</body>
 </html>
-
