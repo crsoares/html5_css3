e7a0a1a9d35ead65f3b39f88a9ef7bd0e9f82254
crysthiano <crysthianophp@gmail.com>
2014-02-20 05:49:26 -0300

    teste layouts

--- cp2/ex2.php
+++ cp2/ex2.php
@@ -1,8 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<!--<meta name="viewport" content="width=device-width,initial-scale=1.0" />-->
+	<!--<meta name="viewport" content="initial-scale=2.0, width=device-width" />-->
+	<meta name="viewport" content="initial-scale=2.0,width=device-width" />
+	<!--<meta name="viewport" content="width=device-width, maximum-scale=3,minimum-scale=0.5" />-->
+	<!--<meta name="viewport" content="initial-scale=1.0,user-scalable=no" />-->
+	<!--<meta name="viewport" content="width=device-width,initial-scale=1.0" />-->
+	
 	<title>Estudos css3 html5</title>
 	<link href="css/main.css" rel="stylesheet" type="text/css" />
 </head>
