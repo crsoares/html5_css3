fabd10fe65d43b3b6adc376f47db947978bcf9f4
crysthiano <crysthiano.php@hotmail.com>
2014-02-21 12:28:05 -0300

    desenvolvimento layout

--- cp2/ex2.php
+++ cp2/ex2.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -21,13 +20,13 @@
 				</ul>
 			</div>
 		</div>
-		<!-- a barra lateral -->
-		<div id="sidebar">
-			<p>aqui é a barra lateral</p>
-		</div>
 		<!-- o conteúdo -->
 		<div id="content">
 			<p>aqui é o conteúdo</p>
+		</div>
+		<!-- a barra lateral -->
+		<div id="sidebar">
+			<p>aqui é a barra lateral</p>
 		</div>
 		<!-- o rodapé -->
 		<div id="footer">
