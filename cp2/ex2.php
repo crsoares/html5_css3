5a5f43d81644a1837e25b81cf922fa8a8fe996fc
crysthiano <crysthiano.php@hotmail.com>
2014-03-06 18:01:39 -0300

    hoje dia 6

--- cp2/ex2.php
+++ cp2/ex2.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta charset="utf-8">
 	<!--<meta name="viewport" content="initial-scale=2.0,width=device-width"/>-->
 	<!--<meta name="viewport" content="width=device-width, maximum-scale=3, minimun-scale=0.5" />-->
 	<!--<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />-->
@@ -13,9 +13,9 @@
 <body>
 	<div id="wrapper">
 		<!-- o cabeçalho e navegação -->
-		<header>
-			<div id="logo">E o vencedor é... </div>
-			<nav>
+		<header role="banner">
+			<div id="logo">E o vencedor <span>é...</span> </div>
+			<nav role="navigation">
 				<ul>
 					<li><a href="#">Por quê?</a></li>
 					<li><a href="#">Sinopse</a></li>
@@ -27,34 +27,33 @@
 			</nav>
 		</header>
 		<!-- o conteúdo -->
-		<div id="content">
-			<article>
-				<header>Um artigo sobre HTML5</header>
-				<nav>
-					<a href="1.html">link relacionado 1</a>
-					<a href="2.html">link relacionado 2</a>
-				</nav>
-				<p>Aqui é o conteúdo do artigo.</p>
-				<footer>Este foi um artigo escrito por Ben Frain.</footer>
-			</article>
-			<h1>Todos os anos <span style="border: 1px solid red;">quando vejo o Oscar Estou chateado...</span></h1>
+		<div id="content" role="main">
+			<img class="oscarMain" src="img/oscar.png" alt="atwi_oscar">
+			<h1>Todos os anos <em>quando vejo o Oscar Estou chateado...</em></h1>
+			<p>que filmes como <b>King Kong</b>, <b>Moulin Rouge</b> e <b>Munique </b> se a estátua, enquanto os verdadeiros heróis cinematográficos perder. Não muito Hollywood é?</p>
+			<p><i>Estamos aqui para colocar as coisas direito.</i></p>
+			<a href="#">Estes devem ter ganho &raquo;</a>
 		</div>
 		<!-- a barra lateral -->
 		<aside>
-			<div class="sideBlock unSung">
-				<h4>Unsung Heroes...</h4>
-				<a href="#"><img src="img/midnightRun.jpg" alt="Midnight Run"></a>
-				<a href="#"><img src="img/wyattEarp.jpg" alt="Wyatt Earp"></a>
-			</div>
-			<div class="sideBlock overHyped">
-				<h4>Overhyped absurdo...</h4>
-				<a href="#"><img src="img/moulinRouge.jpg" alt="Moulin Rouge"></a>
-				<a href="#"><img src="img/kingKong.jpg" alt="King Kong"></a>
-			</div>
+			<section role="complementary">
+				<div class="sideBlock unSung">
+					<h4>Unsung Heroes...</h4>
+					<a href="#"><img src="img/midnightRun.jpg" alt="Midnight Run"></a>
+					<a href="#"><img src="img/wyattEarp.jpg" alt="Wyatt Earp"></a>
+				</div>
+			</section>
+			<section role="complementary">
+				<div class="sideBlock overHyped">
+					<h4>Overhyped absurdo...</h4>
+					<a href="#"><img src="img/moulinRouge.jpg" alt="Moulin Rouge"></a>
+					<a href="#"><img src="img/kingKong.jpg" alt="King Kong"></a>
+				</div>
+			</section>
 		</aside>
 		<!-- o rodapé -->
-		<footer>
-			<p>Aqui está o rodapé</p>
+		<footer role="contentinfo">
+			<p>Nota: nossa opinião é absolutamente correto. Você está errado, mesmo se você acha que está certo. Isso é um fato. Lidar com ele.</p>
 		</footer>
 	</div>
 </body>
